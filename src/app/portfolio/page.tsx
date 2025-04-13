fe3c24a8321a3791ec2ec39d92bef446d8eeefff
“Doommir” <mbookoutvarner@gmail.com>
2025-04-13 12:29:34 -0500

    Enhance project card to make it more clearly clickable with a View Project button

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -130,7 +130,7 @@ export default function PortfolioPage() {
                 
                 {/* Wrap the entire card in a Link if project has a link */}
                 {project.link ? (
-                  <Link href={project.link} className="block h-full">
+                  <Link href={project.link} className="block h-full cursor-pointer">
                     <motion.div 
                       className="p-6 flex flex-col h-full"
                       whileHover={{ y: -8 }}
@@ -161,6 +161,7 @@ export default function PortfolioPage() {
                             width={800}
                             height={600}
                             className="w-full rounded-md hover:shadow-lg transition-shadow duration-300"
+                            priority
                           />
                         </div>
                       )}
@@ -178,6 +179,12 @@ export default function PortfolioPage() {
                             {tag}
                           </motion.span>
                         ))}
+                      </div>
+                      <div className="mt-4 text-primary text-sm font-medium flex items-center">
+                        View Project
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 ml-1" viewBox="0 0 20 20" fill="currentColor">
+                          <path fillRule="evenodd" d="M10.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L12.586 11H5a1 1 0 110-2h7.586l-2.293-2.293a1 1 0 010-1.414z" clipRule="evenodd" />
+                        </svg>
                       </div>
                     </motion.div>
                   </Link>
